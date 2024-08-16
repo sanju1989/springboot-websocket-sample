3e3107706e9a9e7b7fc9aafcded28f778d74abd3
sanjay <sharmaroyl123@gmail.com>
2024-08-16 09:54:40 +0530

    initial commit

---
+++ src/main/java/com/example/websocket_sample/WebsocketSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.websocket_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/websocket_sample/WebsocketSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websocket_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
